b57e9d183af32b3a40cf0ef11c21de4716e6706f
Sri Nikhil Bandi <nivasbandi1234@gmail.com>
2025-09-17 10:47:21 -0400

    Using Render as a Hosting Service.

--- app.py
+++ app.py
@@ -274,4 +274,5 @@ def summary_audio():
         'already_converted': False
     })
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
